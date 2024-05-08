a7b3350c842262e903e23ce5df312d5fce1c3a8e
lorecodez <lorenzo.washington233@gmail.com>
2024-05-08 01:00:49 -0700

    working on list component

--- app/components/List.tsx
+++ app/components/List.tsx
@@ -16,20 +16,44 @@ type Props = {
 
 export default function List({ data }: Props) {
 
-    const [filterList, setFilteredList] = useState([]);
+    const [filterList, setFilteredList] = useState<Item[]>([]);
 
-    const [id, setId] = useState(undefined);
-    const [firstDate, setFirstDate] = useState(undefined);
-    const [lastDate, setLastDate] = useState(undefined);
-    const [name, setName] = useState(undefined);
-    const [firstPrice, setFirstPrice] = useState(undefined);
-    const [lastPrice, setLastPrice] = useState(undefined);
-    const [color, setColor] = useState(undefined);
-    
+    const [id, setId] = useState<string | undefined>(undefined);
+    const [date, setDate] = useState<Date[] | undefined>(undefined);
+    const [name, setName] = useState<string | undefined>(undefined);
+    const [price, setPrice] = useState<number[] | undefined>(undefined);
+    const [color, setColor] = useState<string | undefined>(undefined);
+    const [category, setCategory] = useState<string[] | undefined>(undefined)
     
   return (
     <div>
-      
+        <details>
+            <summary>
+                <h2>Filter</h2>
+            </summary>
+            <form>
+                <input
+                title='Name'
+                value={name}
+                type="text"
+                placeholder='Item Name'
+                onChange={(e) => setName(e.target.value)}
+                />
+                <input
+                title='Id'
+                value={id}
+                type="number"
+                placeholder='Item Id'
+                onChange={(e) => setId(e.target.value)} 
+                />
+                <input
+                title='Minimum Date'
+                />
+            </form>
+        </details>
+        <section>
+
+        </section>
     </div>
   )
 }
