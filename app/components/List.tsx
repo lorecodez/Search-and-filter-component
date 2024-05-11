97720d431b488e99d2137c7680a7089a9eae2b26
lorecodez <lorenzo.washington233@gmail.com>
2024-05-10 17:19:54 -0700

    working on making the code actully work and changed some labeling

--- app/components/List.tsx
+++ app/components/List.tsx
@@ -1,9 +1,9 @@
 'use client'
 import React, { useState } from 'react';
 
-
+//types for data
 type Item = {
-    id: string;
+    id: number;
     name: string;
     createdAt: Date;
     price: number;
@@ -17,41 +17,94 @@ type Props = {
 
 export default function List({ data }: Props) {
 
-    const [filterList, setFilteredList] = useState<Item[]>([]);
+    //creating list of filter params and filtered data array
+    const [filterList, setFilteredList] = useState<{}>({});
+    const [filteredData, setFilterData] = useState<Item[]>(data)
 
-    const [id, setId] = useState<string | undefined>(undefined);
-    const [minDate, setMinDate] = useState<Date | undefined>(undefined);
-    const [maxDate, setMaxDate] = useState<Date | undefined>(undefined);
-    const [name, setName] = useState<string | undefined>(undefined);
-    const [minPrice, setMinPrice] = useState<number | undefined>(undefined);
-    const [maxPrice, setMaxPrice] = useState<number | undefined>(undefined);
-    const [color, setColor] = useState<string | undefined>(undefined);
+    //creating states for filter params
+    const [id, setId] = useState<string | undefined>();
+    const [minDate, setMinDate] = useState<Date | undefined>();
+    const [maxDate, setMaxDate] = useState<Date | undefined>();
+    const [name, setName] = useState<string | undefined>();
+    const [minPrice, setMinPrice] = useState<number | undefined>();
+    const [maxPrice, setMaxPrice] = useState<number | undefined>();
+    const [color, setColor] = useState<string | undefined>();
     const [categories, setCategories] = useState<string[]>([])
+
+    //adding filters and searching function
+    const handleSearch = () => {
+
+        console.log('handleSearch')
+
+        if(id){
+            setFilteredList({...filterList, id})
+            console.log('set id')
+        } else if(name){
+            setFilteredList({...filterList, name})
+            console.log('set name')
+        } else if(minDate){
+            setFilteredList({...filterList, minDate})
+            console.log('set date')
+        } else if(maxDate){
+            // setFilteredList({...filterList, maxDate})
+        } else if(minPrice){
+            setFilteredList({...filterList, minPrice})
+            console.log('set price')
+        } else if(maxPrice){
+            //setFilteredList({...filterList, name})
+        } else if(color){
+            setFilteredList({...filterList, color})
+            console.log('set color')
+        } else if(categories.length > 0){
+            setFilteredList({...filterList, categories})
+            console.log('set categories')
+        }
+
+        setFilterData(data.filter((item) => {
+            console.log('filtering...')
+            for(var key in filterList){
+                if(item[key as keyof Item] === undefined || item[key as keyof Item] != filterList[key as keyof {}]){
+                    return false;
+                }
+
+                console.log(item)
+                
+                return true;
+            }
+        }));
+
+    };
     
   return (
     <div>
         <details>
             <summary>
-                <h2>Filter</h2>
+                <h2 className='font-bold'>Filter Options</h2>
             </summary>
-            <form>
+            <form className='flex flex-col'>
+                <label htmlFor="name">Item Name</label>
                 <input
+                id='name'
                 title='Name'
                 name='name'
                 value={name}
                 type="text"
-                placeholder='Item Name'
+                placeholder='Place enter a name'
                 onChange={(e) => setName(e.target.value)}
                 />
+                <label htmlFor="id">Item Id</label>
                 <input
+                id='id'
                 title='Id'
                 name='id'
                 value={id}
                 type="number"
-                placeholder='Item Id'
+                placeholder='Please enter an ID'
                 onChange={(e) => setId(e.target.value)} 
                 />
+                <label htmlFor="minDate">Min Date</label>
                 <input
+                id='minDate'
                 title='Minimum Date'
                 name='minDate'
                 value={minDate?.toDateString()}
@@ -65,7 +118,9 @@ export default function List({ data }: Props) {
                     setMinDate(e.target.valueAsDate);
                 }}
                 />
+                <label htmlFor="maxDate">Max Date</label>
                 <input
+                id='maxDate'
                 title='Maximum Date'
                 name='maxDate'
                 value={maxDate?.toDateString()}
@@ -79,7 +134,9 @@ export default function List({ data }: Props) {
                     setMaxDate(e.target.valueAsDate);
                 }}
                 />
+                <label htmlFor="minPrice">Min Price</label>
                 <input
+                id='minPrice'
                 title='Minimun Price'
                 name='minPrice'
                 value={minPrice}
@@ -87,7 +144,9 @@ export default function List({ data }: Props) {
                 onChange={(e) => setMinPrice(e.target.valueAsNumber)}
                 placeholder='please enter a number'
                 />
+                <label htmlFor="maxPrice">Max Price</label>
                 <input
+                id='maxPrice'
                 title='Maximun Price'
                 name='maxPrice'
                 value={maxPrice}
@@ -95,7 +154,9 @@ export default function List({ data }: Props) {
                 onChange={(e) => setMaxPrice(e.target.valueAsNumber)}
                 placeholder='please enter a number'
                 />
+                <label htmlFor='color'>Color</label>
                 <input
+                id='Color'
                 title='Color'
                 name='color'
                 value={color}
@@ -111,117 +172,137 @@ export default function List({ data }: Props) {
                 </datalist>
                 <div>
                     <h3>Categories</h3>
-                   <div>
-                   <input
-                    id='fruit'
-                    title='Fruit'
-                    name='fruit'
-                    value={'fruit'}
-                    type='radio'
-                    checked={ categories.includes('fruit') }
-                    onClick={() => {
-                        if( categories.includes('fruit') ){
-                            setCategories(categories.filter(item => item != 'fruit'))
-                        } else {
-                            setCategories([...categories, 'fruit'])
-                        }
-                    }}
-                    />
-                    <label htmlFor='fruit'>fruit</label>
-                    <input
-                    id='sale'
-                    title='Sale'
-                    name='sale'
-                    value={'sale'}
-                    type='radio'
-                    checked={ categories.includes('sale') }
-                    onClick={() => {
-                        if( categories.includes('sale') ){
-                            setCategories(categories.filter(item => item != 'sale'))
-                        } else {
-                            setCategories([...categories, 'sale'])
-                        }
-                    }}
-                    />
-                    <label htmlFor='sale'>sale</label>
-                    <input 
-                    type="radio" 
-                    name="citrus" 
-                    id="citrus"
-                    title='Citrus'
-                    value={'citrus'} 
-                    checked={ categories.includes('citrus')}
-                    onClick={() => {
-                        if( categories.includes('citrus') ){
-                            setCategories(categories.filter(item => item != 'citrus'))
-                        } else {
-                            setCategories([...categories, 'citrus'])
-                        }
-                    }}
-                    />
-                    <label htmlFor="citrus">Citrus</label>
-                    <input 
-                    type="radio" 
-                    name="organic" 
-                    id="organic"
-                    title='Organic'
-                    value={'organic'}
-                    checked={ categories.includes('organic')}
-                    onClick={() => {
-                        if( categories.includes('organic') ){
-                            setCategories(categories.filter(item => item != 'organic'))
-                        } else {
-                            setCategories([...categories, 'organic'])
-                        }
-                    }}
-                    />
-                    <label htmlFor="organic">Organic</label>
-                    <input 
-                    type="radio"
-                    name='tropical'
-                    id='tropical'
-                    title='Tropical'
-                    value={'tropical'}
-                    checked={ categories.includes('tropical')}
-                    onClick={() => {
-                        if( categories.includes('tropical') ){
-                            setCategories(categories.filter(item => item != 'tropical'))
-                        } else {
-                            setCategories([...categories, 'tropical'])
-                        }
-                    }}
-                    />
-                    <label htmlFor="tropical">Tropical</label>
-                    <input 
-                    type="radio" 
-                    name="vegtable" 
-                    id="vegtable"
-                    title='Vegtable'
-                    value={'vegtable'}
-                    checked={ categories.includes('vegtable') }
-                    onClick={() => {
-                        if( categories.includes('vegtable') ){
-                            setCategories(categories.filter(item => item != 'vegtable'))
-                        } else {
-                            setCategories([...categories, 'vegtable'])
-                        }
-                    }}
-                    />
+                    <div>
+                        <input
+                        id='fruit'
+                        title='Fruit'
+                        name='fruit'
+                        value={'fruit'}
+                        type='radio'
+                        checked={ categories.includes('fruit') }
+                        onClick={() => {
+                            if( categories.includes('fruit') ){
+                                setCategories(categories.filter(item => item != 'fruit'))
+                            } else {
+                                setCategories([...categories, 'fruit'])
+                            }
+                        }}
+                        />
+                        <label className='mr-1' htmlFor='fruit'>fruit</label>
+                        <input
+                        id='sale'
+                        title='Sale'
+                        name='sale'
+                        value={'sale'}
+                        type='radio'
+                        checked={ categories.includes('sale') }
+                        onClick={() => {
+                            if( categories.includes('sale') ){
+                                setCategories(categories.filter(item => item != 'sale'))
+                            } else {
+                                setCategories([...categories, 'sale'])
+                            }
+                        }}
+                        />
+                        <label className='mr-1' htmlFor='sale'>sale</label>
+                        <input 
+                        type="radio" 
+                        name="citrus" 
+                        id="citrus"
+                        title='Citrus'
+                        value={'citrus'} 
+                        checked={ categories.includes('citrus')}
+                        onClick={() => {
+                            if( categories.includes('citrus') ){
+                                setCategories(categories.filter(item => item != 'citrus'))
+                            } else {
+                                setCategories([...categories, 'citrus'])
+                            }
+                        }}
+                        />
+                        <label className='mr-1' htmlFor="citrus">Citrus</label>
+                        <input 
+                        type="radio" 
+                        name="organic" 
+                        id="organic"
+                        title='Organic'
+                        value={'organic'}
+                        checked={ categories.includes('organic')}
+                        onClick={() => {
+                            if( categories.includes('organic') ){
+                                setCategories(categories.filter(item => item != 'organic'))
+                            } else {
+                                setCategories([...categories, 'organic'])
+                            }
+                        }}
+                        />
+                        <label className='mr-1' htmlFor="organic">Organic</label>
+                        <input 
+                        type="radio"
+                        name='tropical'
+                        id='tropical'
+                        title='Tropical'
+                        value={'tropical'}
+                        checked={ categories.includes('tropical')}
+                        onClick={() => {
+                            if( categories.includes('tropical') ){
+                                setCategories(categories.filter(item => item != 'tropical'))
+                            } else {
+                                setCategories([...categories, 'tropical'])
+                            }
+                        }}
+                        />
+                        <label className='mr-1' htmlFor="tropical">Tropical</label>
+                        <input 
+                        type="radio" 
+                        name="vegtable" 
+                        id="vegtable"
+                        title='Vegtable'
+                        value={'vegtable'}
+                        checked={ categories.includes('vegtable') }
+                        onClick={() => {
+                            if( categories.includes('vegtable') ){
+                                setCategories(categories.filter(item => item != 'vegtable'))
+                            } else {
+                                setCategories([...categories, 'vegtable'])
+                            }
+                        }}
+                        />
+                        <label className='mr-1' htmlFor="vegtable">Vegtable</label>
                    </div>
                 </div>
+                <button
+                title='Search'
+                name='search'
+                type='button'
+                onClick={handleSearch}
+                className='rounded-2xl p-2 border border-black'
+                >
+                    Search
+                </button>
             </form>
         </details>
         <section>
-                <p>{name}</p>
-                <p>{id}</p>
-                <p>{minDate?.toLocaleDateString()}</p>
-                <p>{maxDate?.toLocaleDateString()}</p>
-                <p>{minPrice}</p>
-                <p>{maxPrice}</p>
-                <p>{color}</p>
-                <p>
-                    {categories.map((category) => category)}
-                </p>
+                <h3 className='font-bold'>Selected Filters</h3>
+                <p>Name: {name}</p>
+                <p>Id: {id}</p>
+                <p>Min Date: {minDate?.toLocaleDateString()}</p>
+                <p>Max Date: {maxDate?.toLocaleDateString()}</p>
+                <p>Min price: {minPrice}</p>
+                <p>Max Price: {maxPrice}</p>
+                <p>Color: {color}</p>
+                <p>Categories:</p>
+                <ul className='flex flex-wrap gap-2'>
+                    {categories.map((category, index) => <li key={index} >{category}</li>)}
+                </ul>
+        </section>
+        <section>
+            <h3 className='font-bold'>Data</h3>
+            <ul>
+                {filteredData.map((item, index) =>
+                    <li key={index}>{JSON.stringify(item)}</li>
+                )}
+            </ul>
         </section>
     </div>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 'use server'
 import { lazy, Suspense } from "react";
+import { data } from "@/data";
 
 const List = lazy(() => import('@/app/components/List').then( module => {
     return {default: module.default}
@@ -10,7 +11,7 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Suspense fallback={<p>Loading...</p>}>
-        <List data={[]}/>
+        <List data={data}/>
       </Suspense>
     </main>
   );
